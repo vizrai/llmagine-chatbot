b5d39311bbce76e17917d5afe61f6601618a448e
vizrai <info@vizr.ai>
2024-08-21 16:06:23 -0600

    Create index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatBox from '../components/ChatBox';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Chat App</h1>
+            <ChatBox />
+        </div>
+    );
+};
+
+export default HomePage;
